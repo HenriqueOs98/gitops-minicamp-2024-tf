ddf82ff226e20d2db771b54d33d4b1106e4b96db
hickos <henriqueos98@outlook.com>
2024-10-12 18:04:11 -0300

    correct format

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.71.0"
     }
   }
 
-  required_version = ">= 1.9.7"
+  required_version = ">= 1.9.6"
 
 }
 
